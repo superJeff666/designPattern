f60a291d976346bb82062bd607f6030fb52ea503
Jeff <982855239@qq.com>
2020-05-18 22:39:32 +0800

    增加迭代器模式

--- es6/src/index.js
+++ es6/src/index.js
@@ -200,7 +200,7 @@
 //     this.subject.attach(this);
 //   }
 //   update() {
-//     console.log(`${this.name} update,state:${this.subject.getState()}`)  
+//     console.log(`${this.name} update,state:${this.subject.getState()}`)
 //   }
 // }
 
@@ -213,3 +213,60 @@
 // s.setState(1);
 // s.setState(2);
 // s.setState(3);
+
+//----------分割线---------------
+//迭代器模式
+
+// class Iterator {
+//   constructor(container) {
+//     this.list = container.list;
+//     this.index = 0;
+//   }
+//   next() {
+//     if (this.hasNext()) {
+//       return this.list[this.index++];
+//     }
+//     return null;
+//   }
+//   hasNext() {
+//     if (this.index >= this.list.length) {
+//       return false;
+//     }
+//     return true;
+//   }
+// }
+
+// class Container {
+//   constructor(list) {
+//     this.list = list;
+//   }
+//   //生成遍历器
+//   getIterator() {
+//     return new Iterator(this);
+//   }
+// }
+
+// //测试
+// let arr = [1,2,3,4,5,6];
+// let container = new Container(arr);
+// let iterator = container.getIterator();
+// while(iterator.hasNext()) {
+//   console.log(iterator.next());
+// }
+
+//迭代器模式
+
+function each(data) {
+    for(let item of data) {
+        console.log(item);
+    }
+}
+let arr = [1,2,3,4];
+let nodeList = document.getElementsByTagName('p');
+let m = new Map();
+m.set('a',100);
+m.set('b',100);
+
+each(arr);
+each(nodeList);
+each(m);
